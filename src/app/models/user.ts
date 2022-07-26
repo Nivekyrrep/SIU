d7dd32df21c72278a92d159cca2d866cebb8cd6a
kperry <kperry@xpanxion.com>
2022-07-26 14:13:51 -0400

    commit 	new file:   src/app/models/LiveReport.ts 	new file:   src/app/models/Report.ts 	new file:   src/app/models/ReportConfig.ts 	new file:   src/app/models/user.ts

---
+++ src/app/models/user.ts
@@ -0,0 +1,11 @@
+export class User {
+    USERNAME!: number;
+    firstName!: string;
+    lastName!: string;
+    email!: string;
+    phoneNumber!: string;
+    userCreated!: number;
+    userModified!: number;
+    userStatus!: number;
+    password!: string;
+}
